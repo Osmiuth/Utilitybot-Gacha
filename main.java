d301d6087caacb79c293e42fb0b459800476342f
joel <joel@localhost.localdomain>
2025-02-11 10:51:10 -0500

    Remodified and refactored some parts, made an unfinished utility-based bot, created full environment, and fixed some bugs on luck-based percentage.

--- main.java
+++ main.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.LinkedList;
 
 /**
  * EXPLANATION:
@@ -29,45 +30,68 @@ class Game {
     public void setTime(int n){
         this.Time = n;
     }
+
 }
 
-
 class Wheel {
-    private int PayOff = 0;
-    private float PayOffChance = 0;
-    private float CurrentChance = 0;
-    Random rand = new Random();
+        private int PayOff = 0;
+        private float PayOffChance = 0;
+        private float CurrentChance = 0;
+        Random rand = new Random();
 
-    public int getPayOff(){ //Getter method
-        return this.PayOff;
-    }
+        public int getPayOff(){ //Getter method
+            return this.PayOff;
+        }
 
-    public void setPayOff(int n){ //Setter method
-        this.PayOff = n;
-    }
+        public void setPayOff(int n){ //Setter method
+            this.PayOff = n;
+        }
 
-    public float getPayOffChance(){ //Getter method
-        return this.PayOffChance;
-    }
+        public float getPayOffChance(){ //Getter method
+            return this.PayOffChance;
+        }
 
-    public void setPayOffChance(float n){ //Setter method
-        this.PayOffChance = n;
-    }
+        public void setPayOffChance(float n){ //Setter method
+            this.PayOffChance = n;
+        }
 
-    public boolean roll() { //Rolling function 
-        this.CurrentChance = rand.nextFloat(100);
-        return this.CurrentChance >= PayOffChance *100;
-    }
-
+        public void roll(Game game) { //Rolling function
+            game.setMoney(game.getMoney()-1);
+            game.setTime(game.getTime()-10);
+            this.CurrentChance = rand.nextFloat(100);
+            if (this.CurrentChance <= PayOffChance *100){
+                System.out.println(this.CurrentChance);
+                game.setMoney(game.getMoney()+this.PayOff);
+            } 
+        }
 }
 
 class MachineBot {
+    private String action = "none";
+    private LinkedList<Integer> LWheelA = new LinkedList<Integer>();
+    private LinkedList<Integer> LWheelB = new LinkedList<Integer>();
 
-/**
- * This is where we put the utility-based agent's functions here.
- * 
- * 
- */
+    public LinkedList<Integer> getListA() {
+        return this.LWheelA;
+    }
+
+    public LinkedList<Integer> getListB() {
+        return this.LWheelB;
+    }
+
+    public void solve(Game game, Wheel wheelA, Wheel wheelB) {
+        if (action.equals("none") && (LWheelA.size() < 5)) {
+            this.action = "rolling";
+            LWheelA.add(game.getMoney());
+            wheelA.roll(game);
+            this.action = "none";
+        } else if (LWheelA.size() >= 5 && (action.equals("none"))) {
+            this.action = "rolling";
+            LWheelB.add(game.getMoney());
+            wheelB.roll(game);
+            this.action = "none";
+        } 
+    }
 
 
 }
@@ -77,18 +101,26 @@ public class main {
 public static void main(String[] args){
     /**
      * 
-     * Added instances for testing purposes.
-     * 
-     */
+     * Added instances for testing purposes./**
+ *
+ * 
+ * 
+ */
     Game game = new Game();
     game.setMoney(100);
-    game.setTime(3600);
+    game.setTime(100);
+    Wheel wheelA = new Wheel();
+    wheelA.setPayOff(5);
+    wheelA.setPayOffChance((float) 0.75);
+    Wheel wheelB = new Wheel();
+    wheelB.setPayOff(100);
+    wheelB.setPayOffChance((float) 0.1);
+    MachineBot bot = new MachineBot();
 
-    Wheel wheel1 = new Wheel();
-    wheel1.setPayOff(10);
-    wheel1.setPayOffChance((float) 0.65);
-    System.out.println(wheel1.roll());
+    while (game.getMoney() > 0 && game.getTime() > 0){
+        bot.solve(game, wheelA, wheelB);
+    }  
+
+    System.out.println(bot.getListB());
 }
-
 }
-
